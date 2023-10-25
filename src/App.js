e60531bca6008d908e1e052bba71ea978ffec7bf
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2023-10-26 00:50:24 +0600

    add register login blood requset and firebase
    add register login blood requset and firebase stuf
    and also someting

--- src/App.js
+++ src/App.js
@@ -1,65 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Page/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Blood_Request from './Page/Blood_Request';
+import Login from './Page/Login';
+import Register from './Page/Register';
+import Search_Donors from './Page/Search_Donors';
+import Error_404 from './Page/Error_404';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
 
 function App() {
+  // Custom theme fo matarial theme Primary Color
+  const customTheme = createTheme({
+    palette: {
+      primary: {
+        main: '#c03c38', 
+      },
+    },
+  });
 
-    function donateNow() {
-        alert("Thank you for your willingness to donate blood. Your contribution can save lives!");
-    }
-    
+
+
+
   return (
     <div>
-    <header>
-        <h1>Blood Donation </h1>
-        <nav>
-            <ul>
-                <li className="inline-block mx-2"><a href="#" className="text-white">Home</a></li>
-                <li className="inline-block mx-2"><a href="#" className="text-white">SearchDonor</a></li>
-                <li className="inline-block mx-2"><a href="#" className="text-white">RequestDonor</a></li>
-                <li className="inline-block mx-2"><a href="#" className="text-white">Register</a></li>
-                <li className="inline-block mx-2"><a href="#" className="text-white">Login</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <section id="hero">
-        <h2>Welcome to our Blood Donation </h2>
-        <p>Help save lives by donating blood. Your contribution can make a difference.</p>
-        <button className="btn" onClick={donateNow}>Donate Now</button>
-    </section>
-
-    <section id="about">
-        <h2>About Us</h2>
-        <p>We are a non-profit organization dedicated to saving lives by providing a safe and efficient blood donation service.</p>
-    </section>
-    
-
-    <section id="donors">
-        <h2>Our Donors</h2>
-        
-    </section>
-
-    <section id="recipients">
-        <h2>Recipients in Need</h2>
-        
-    </section>
-
-    <section id="contact">
-        <h2>Contact Us</h2>
-        <p>If you have any questions or need assistance, please feel free to contact us.</p>
-    </section>
-
-    <footer>
-        <p>&copy; 2023 Blood Donation </p>
-    </footer>
-
-   
+      
+      <ThemeProvider theme={customTheme}>
 
 
-    
-</div>
+        <Router>
+          <Switch>
+            
+
+            <Route path='/Blood_Request'>
+              <Blood_Request></Blood_Request>
+
+            </Route>
+            <Route path='/Login'>
+              <Login></Login>
+
+            </Route>
+            <Route path='/Register'>
+              <Register></Register>
+
+            </Route>
+            <Route path='/Search_Donors'>
+              <Search_Donors></Search_Donors>
+
+            </Route>
+            <Route exact path='/'>
+              <Home></Home>
+            </Route>
+
+            <Route path='*'>
+              <Error_404></Error_404>
+
+            </Route>
+
+          </Switch>
+        </Router>
+
+
+      </ThemeProvider>
+
+    </div>
   );
- 
+
 }
 
 export default App;
